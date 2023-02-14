6d3e2fc5247a92035651b262c920485ffff27900
PatLaCisay <lucasbarreaupub@gmail.com>
2023-02-14 09:53:48 +0100

    amélioration de l'animation de tri_shell

--- Tri_shell/tri_shell.js
+++ Tri_shell/tri_shell.js
@@ -44,7 +44,7 @@ function Deplace_Horizontal(Barre)
 				j=j+4;
 			}
 		}
-		setInterval(cycle, 4); 	// lance le cycle chaque 45 millisecondes	
+		setInterval(cycle, 45); 	// lance le cycle chaque 45 millisecondes	
 }
 
 function per(table,i,j)
@@ -59,7 +59,6 @@ function Dessin(Barre)
 {
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
-	Barre.x=20;
 	var j = Barre.x; // variable fonction du temps
 	var cycle = function() 
 	{
@@ -73,7 +72,7 @@ function Dessin(Barre)
 			j=j+2;
 		}
 	}
-	setInterval(cycle, 6); 
+	setInterval(cycle, 45); 
 }
 
 function tri_insertion(tableau, gap, debut) {
@@ -94,28 +93,22 @@ function tri_insertion(tableau, gap, debut) {
 
 function cycle_shell()
 {
-	console.log(gaps.length);
 	if (index<gaps.length) {
-		var gap = gaps[index];
+		gap = gaps[index];
 		
-		for (var debut = 0; debut < gap; debut += 1) {
-			
+		if (debut < gap) {
 			for (var i = gap + debut; i < tab.length; i += gap) {
 
 				min = tab[i].dx;
 				j = i;
+				//Deplace_Horizontal(tab[j]);
 				while (j > 0 && tab[j - gap].dx > min) 
 				{ 	
-					var deplace = function(){
-						Deplace_Horizontal(tab[j-gap]);
-						Deplace_Horizontal(tab[j]);
-					}
-					setTimeout(deplace,4900);
-
-
+					//Deplace_Horizontal(tab[j-gap]);
 					Dessin(tab[j-gap]);
 					tab[j].dx = tab[j - gap].dx;
 					Dessin(tab[j]);
+					
 
 					j -= gap;
 
@@ -128,20 +121,22 @@ function cycle_shell()
 				Dessin(tab[j]);
 			}
 		}
-		
+		debut += 1;
+		if(debut==gap)
+		{
+			debut=0;
+			index++;
+		}
 	}
 
-	index++;
-	tab.forEach(element => {
-		console.log(element.dx);
-	});
 }
 
 function Tri_Shell(tab)
 {
 	index=0; 
+	debut=0;
 	gaps=[6, 4, 3, 2, 1];
-	rep=setInterval(cycle_shell,9000);		
+	rep=setInterval(cycle_shell,5000);		
 }
 
 function alert_stop()
@@ -174,51 +169,10 @@ function stop()
 
 function continuer()
 {
-	rep1= setInterval(cycle_Shell,14000);
+	rep1= setInterval(cycle_shell,14000);
 }
 
 function Lance_tri_shell()
 {
 	Tri_Shell(tab);
-}
-
-////////////
-
-var tab1;
-
-function tri_insertion(tableau, gap, debut) {
-	var en_cours, j;
-
-	for (var i = gap + debut; i < tableau.length; i += gap) {
-		en_cours = tableau[i];
-		j = i;
-		while (j > 0 && tableau[j - gap] > en_cours) {
-			tableau[j] = tableau[j - gap];
-			j -= gap;
-			console.log("j="+j+" ; gap="+gap)
-		}
-
-		tableau[j] = en_cours;
-	}
-}
-
-function tri_shell(tableau) {
-	tab1=[6, 4, 3, 2, 1];
-	for (i=0; i<tab1.length; i++) {
-		var gap = tab1[i];
-
-		for (var debut = 0; debut < gap; debut ++) {
-			tri_insertion(tableau, gap, debut);
-		}
-	}
-
-	return tableau;
-}
-
-tab1 = [701, 301, 132, 57, 23, 10, 4, 1];
-console.log(tri_shell(tab1));
-
-function recharger()
-{
-	location.reload();
 }
